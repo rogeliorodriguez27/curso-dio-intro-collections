e85d0ac295815ab3fbc75b5bad37c370bad546b1
rogeliorodriguez27 <rogelio23rodriguez@gmail.com>
2024-06-05 23:08:27 -0300

    Exercicio 1 Set, passo a,b,c,d

--- src/br/com/dio/collection/set/ExercicioProposto01.java
+++ src/br/com/dio/collection/set/ExercicioProposto01.java
@@ -1,10 +1,13 @@
 package br.com.dio.collection.set;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Crie uma conjunto contendo as cores do arco-íris e:
 a) Exiba todas as cores o arco-íris uma abaixo da outra;
+# vermelho, laranja, amarelo, verde, azul, azul marinho ( índigo ) e violeta
 b) A quantidade de cores que o arco-íris tem;
 c) Exiba as cores em ordem alfabética;
 d) Exiba as cores na ordem inversa da que foi informada;
@@ -15,51 +18,20 @@ h) Confira se o conjunto está vazio;
  */
 public class ExercicioProposto01 {
     public static void main(String[] args) {
-        System.out.println("Crie uma conjunto contendo as cores do arco-íris: ");
-        Set<String> coresArcoIris = new HashSet<>();
-        coresArcoIris.add("violeta");
-        coresArcoIris.add("anil");
-        coresArcoIris.add("azul");
-        coresArcoIris.add("verde");
-        coresArcoIris.add("amarelo");
-        coresArcoIris.add("laranja");
-        coresArcoIris.add("vermelho");
-        System.out.println(coresArcoIris);
 
-        System.out.println("Exiba todas as cores o arco-íris uma abaixo da outra: ");
-        for (String cor : coresArcoIris) {
-            System.out.println(cor);
-        }
+        Set<String> arcoIris = new LinkedHashSet<>(Arrays.asList("vermelho", "laranja", "amarelo", "verde", "azul", "indigo", "violeta"));
 
-        System.out.println("A quantidade de cores que o arco-íris tem: " + coresArcoIris.size());
+        System.out.println("Exibindo cores do arco-iris:");
+        arcoIris.forEach(System.out::println);
+        System.out.print("Quantidade de cores:" + arcoIris.size()+"\n");
 
-        System.out.println("Exiba as cores em ordem alfabética: ");
-        Set<String> coresArcoIris2 = new TreeSet<>(coresArcoIris);
-        System.out.println(coresArcoIris2);
+        Set<String> arcoIrisMap = new TreeSet<>(arcoIris);
+        System.out.println("Em orden alfabética: "+ arcoIrisMap);
 
-        System.out.println("Exiba as cores na ordem inversa da que foi informada ");
-        Set<String> coresArcoIris3 = new LinkedHashSet<>(
-                Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
-        System.out.println(coresArcoIris3);
-        List<String> coresArcoIrisList = new ArrayList<>(coresArcoIris3);
-        Collections.reverse(coresArcoIrisList);
-        System.out.println(coresArcoIrisList);
+        System.out.println("Mostrar em ordem inverso ao inicial: ");
 
-        System.out.println("Exiba todas as cores que começam com a letra ”v”: ");
-        for (String cor: coresArcoIris) {
-            if(cor.startsWith("v")) System.out.println(cor);
-        }
-
-        System.out.println("Remova todas as cores que não começam com a letra “v”: ");
-        Iterator<String> iterator2 = coresArcoIris.iterator();
-        while (iterator2.hasNext()) {
-            if (!iterator2.next().startsWith("v")) iterator2.remove();
-        }
-        System.out.println(coresArcoIris);
-
-        System.out.println("Limpe o conjunto: ");
-        coresArcoIris.clear();
-
-        System.out.println("Confira se o conjunto está vazio: " + coresArcoIris.isEmpty());
+        List<String> arcoIrisReverse = new ArrayList<>(arcoIris);
+        Collections.reverse(arcoIrisReverse);
+        System.out.println(arcoIrisReverse);
     }
 }
